e4b2f61fe13ac09674f1ede6f91b62a1996c0953
Metekillot <49173900+Metekillot@users.noreply.github.co>
2025-02-13 16:11:10 -0500

    i'm going to be making a bunch of commits that look the same just so i can continue testing CI god forgive me

--- dependencies.sh
+++ dependencies.sh
@@ -8,7 +8,7 @@ export BYOND_MAJOR=515
 export BYOND_MINOR=1645
 
 #rust_g git tag
-export RUST_G_VERSION=3.3.0
+export RUST_G_VERSION=3.5.1
 
 #node version
 export NODE_VERSION=12
@@ -17,10 +17,10 @@ export NODE_VERSION_PRECISE=12.20.0
 export NODE_VERSION_LTS=22.11.0
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.6
+export SPACEMAN_DMM_VERSION=suite-1.9
 
 # Extools git tag
 export EXTOOLS_VERSION=v0.0.7
 
 # Python version for mapmerge and other tools
-export PYTHON_VERSION=3.6.8
+export PYTHON_VERSION=3.9.0
