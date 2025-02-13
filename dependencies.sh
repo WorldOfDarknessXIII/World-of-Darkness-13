351406f26a9393d3cd776a6a144ed65f26fc295c
Metekillot <49173900+Metekillot@users.noreply.github.co>
2025-02-13 15:50:15 -0500

    i'm going to be making a bunch of commits that look the same just so i can continue testing CI god forgive me

--- dependencies.sh
+++ dependencies.sh
@@ -14,6 +14,8 @@ export RUST_G_VERSION=3.3.0
 export NODE_VERSION=12
 export NODE_VERSION_PRECISE=12.20.0
 
+export NODE_VERSION_LTS=22.11.0
+
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.6
 
