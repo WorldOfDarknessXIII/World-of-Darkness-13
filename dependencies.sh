9c86d79c4c51e47458ab14b061fef35cde1b8b6b
Joshua Kidder <49173900+Metekillot@users.noreply.github.com>
2025-02-13 19:44:01 -0500

    ci

--- dependencies.sh
+++ dependencies.sh
@@ -24,3 +24,9 @@ export EXTOOLS_VERSION=v0.0.7
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.9.0
+
+#hypnagogic repo
+export CUTTER_REPO=spacestation13/hypnagogic
+
+#hypnagogic git tag
+export CUTTER_VERSION=v4.0.0
